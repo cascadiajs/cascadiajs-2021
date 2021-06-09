4e964c1ed3bd3833492317c507050297c5eb79cf
Carter Rabasa <carter.rabasa@gmail.com>
2021-06-09 07:31:08 -0700

    trying to fix autoplay video in mobile safari

--- src/views/index.js
+++ src/views/index.js
@@ -7,16 +7,16 @@ module.exports = function IndexView () {
             <h2>CascadiaJS 2021 is going hybrid!</h2>
             <div id="preview-carousel">
                 <div class="carousel">
-                    <video aria-label="Video from https://twitter.com/Not_Woods/status/1301285874697551877?s=20" src="https://video.twimg.com/tweet_video/Eg8YmKXVoAAFETv.mp4" type="video/mp4"  muted autoplay loop></video>
-                    <video aria-label="Video from https://twitter.com/monteslu/status/1301183840648605703?s=20" src="https://video.twimg.com/tweet_video/Eg67xFHUMAAUkKl.mp4" type="video/mp4"  muted autoplay loop></video>
-                    <video aria-label="Video from https://twitter.com/BronwenEvans11/status/1301264949822464002?s=20" src="https://video.twimg.com/tweet_video/Eg8FBOhVoAAaN-d.mp4" type="video/mp4"  muted autoplay loop></video>
-                    <video aria-label="Embedded video" src="https://video.twimg.com/tweet_video/Eg8C0WjWkAE2MQr.mp4" type="video/mp4"  muted autoplay loop></video>
-                    <video aria-label="Embedded video" src="https://video.twimg.com/tweet_video/Eg7BZOEWkAEAYtJ.mp4" type="video/mp4"  muted autoplay loop></video>
-                    <video aria-label="Embedded video" src="https://video.twimg.com/tweet_video/Eg7AXB1VkAASsoj.mp4" type="video/mp4"  muted autoplay loop></video>
-                    <video aria-label="Embedded video" src="https://video.twimg.com/tweet_video/Eg8tbzOUYAApIiH.mp4" type="video/mp4"  muted autoplay loop></video>
-                    <video aria-label="Embedded video" src="https://video.twimg.com/tweet_video/Eg80-8TUYAArvpl.mp4" type="video/mp4"  muted autoplay loop></video>
-                    <video aria-label="Embedded video" src="https://video.twimg.com/tweet_video/Eg8xVQoVkAUIxiK.mp4" type="video/mp4"  muted autoplay loop></video>
-                    <video aria-label="Embedded video" src="https://video.twimg.com/tweet_video/Eg8WNvnVkAA7ibv.mp4" type="video/mp4"  muted autoplay loop></video>
+                    <video aria-label="Video from https://twitter.com/Not_Woods/status/1301285874697551877?s=20" src="https://video.twimg.com/tweet_video/Eg8YmKXVoAAFETv.mp4" type="video/mp4"  muted autoplay loop playsinline></video>
+                    <video aria-label="Video from https://twitter.com/monteslu/status/1301183840648605703?s=20" src="https://video.twimg.com/tweet_video/Eg67xFHUMAAUkKl.mp4" type="video/mp4"  muted autoplay loop playsinline></video>
+                    <video aria-label="Video from https://twitter.com/BronwenEvans11/status/1301264949822464002?s=20" src="https://video.twimg.com/tweet_video/Eg8FBOhVoAAaN-d.mp4" type="video/mp4"  muted autoplay loop playsinline></video>
+                    <video aria-label="Embedded video" src="https://video.twimg.com/tweet_video/Eg8C0WjWkAE2MQr.mp4" type="video/mp4"  muted autoplay loop playsinline></video>
+                    <video aria-label="Embedded video" src="https://video.twimg.com/tweet_video/Eg7BZOEWkAEAYtJ.mp4" type="video/mp4"  muted autoplay loop playsinline></video>
+                    <video aria-label="Embedded video" src="https://video.twimg.com/tweet_video/Eg7AXB1VkAASsoj.mp4" type="video/mp4"  muted autoplay loop playsinline></video>
+                    <video aria-label="Embedded video" src="https://video.twimg.com/tweet_video/Eg8tbzOUYAApIiH.mp4" type="video/mp4"  muted autoplay loop playsinline></video>
+                    <video aria-label="Embedded video" src="https://video.twimg.com/tweet_video/Eg80-8TUYAArvpl.mp4" type="video/mp4"  muted autoplay loop playsinline></video>
+                    <video aria-label="Embedded video" src="https://video.twimg.com/tweet_video/Eg8xVQoVkAUIxiK.mp4" type="video/mp4"  muted autoplay loop playsinline></video>
+                    <video aria-label="Embedded video" src="https://video.twimg.com/tweet_video/Eg8WNvnVkAA7ibv.mp4" type="video/mp4"  muted autoplay loop playsinline></video>
                 </div>
             </div>
             <div class="narrow">
