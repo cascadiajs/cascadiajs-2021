fb029dda6aa8a842beb6267fc3c02a5793316740
Peter deHaan <pdehaan@hey.com>
2021-08-29 16:48:38 -0700

    Fix some invalid closing H1 tags

--- src/views/index.js
+++ src/views/index.js
@@ -36,7 +36,7 @@ let OrganizersContainer = require('./components/organizers')
             </div>
         </section>
         <section id="speakers" class="landing">
-            <h1>Speakers</h2>
+            <h1>Speakers</h1>
             ${ speakersContainer }
         </section>
         <section id="sponsors" class="landing">
@@ -47,7 +47,7 @@ let OrganizersContainer = require('./components/organizers')
             </div>
         </section>
         <section id="organizers" class="landing">
-            <h1>Organizers</h2>
+            <h1>Organizers</h1>
             ${ organizersContainer }
         </section>
     </div>
