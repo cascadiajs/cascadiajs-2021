dce069296721d5f2baa0c5b2410cce7186da506e
Carter Rabasa <carter.rabasa@gmail.com>
2021-09-13 13:07:35 -0700

    funnier 404

--- src/views/404.js
+++ src/views/404.js
@@ -2,7 +2,9 @@ let Layout = require('./layout')
 
 module.exports = function notFound () {
     // Return 404
-    let content = `<div>404, not found :(</div>`
+    let content = `<video autoplay muted loop width="100%">
+      <source type="video/mp4" src="https://media.giphy.com/media/jkZtSdwKOx05BOlapR/source.mp4?cid=ecf05e474kgt0ax6stge6re5otcyqmhf7p7bx1ncirx3lp2q&rid=source.mp4&ct=g">
+    </video>`
     let html = Layout({ content })
     return {
       html,
