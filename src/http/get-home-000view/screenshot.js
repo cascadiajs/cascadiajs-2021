f71fe6eac283b52616959738a29c038067568dde
Carter Rabasa <carter.rabasa@gmail.com>
2021-09-12 21:09:25 -0700

    sadness

--- src/http/get-home-000view/screenshot.js
+++ src/http/get-home-000view/screenshot.js
@@ -1,6 +1,6 @@
 const AWS = require('aws-sdk')
 const chromium = require('chrome-aws-lambda')
-require('puppeteer-core')
+const pc = require('puppeteer-core')
 const baseUrl = require('@architect/shared/utils/base-url')()
 
 module.exports = async function screencap({ ticket }) {
@@ -10,6 +10,12 @@ module.exports = async function screencap({ ticket }) {
     //let height = 628
     //let width = 1200
     //let deviceScaleFactor = 1
+
+    // sadness, delete when possible
+    if (pc) {
+      console.log('puppeteer-core loaded')
+    }
+
     try {
       browser = await chromium.puppeteer.launch({
         args: chromium.args,
