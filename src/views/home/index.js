c985b913fea6fc0780eef27dc2fc834ad211d119
Carter Rabasa <carter.rabasa@gmail.com>
2021-09-13 18:38:04 -0700

    only allow holders of conference tickets to be listed in the Directory

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -13,16 +13,19 @@ module.exports = async function Index({ ticket }) {
                     <li><b>Conference:</b> ${ ticket.conference == 'Y' ? "Yes" : "No" }</li>
                     <li><b>Goodie Box:</b> ${ ticket.hoodie === 'Y' ? "Yes" : "No" }</li>
                 </ul>
-                ${ ticket.hoodie === 'Y' ?
-                    `<p><span class="highlight warning">Note: Deadline to redeem Goodie Box is 9/17</span></p>
-                     <div class="cta"><a href="/home/hoodies">Redeem Goodie Box</a></div>` : '' }
-                <h2>Conference Directory</h2>
+                ${ ticket.hoodie === 'Y'
+                    ? `<p><span class="highlight warning">Note: Deadline to redeem Goodie Box is 9/17</span></p>
+                     <div class="cta"><a href="/home/hoodies">Redeem Goodie Box</a></div>`
+                    : '' }
+                ${ ticket.conference === 'Y'
+                ? /*html*/`<h2>Conference Directory</h2>
                 ${ ticket.github && ticket.github !== ''
                     ? `<p>You have been added to the Conference Directory ✅<p>
                     <p>We have also generated a <a href="/tickets/${ ticket.number }" target="_blank">virtual ticket</a> that you can share on social media. Anyone who registers via your virtual ticket page gets 10% off!</p>`
                     : `<p>Let folks know you're attending CascadiaJS this year! We use <a target="_blank" href="https://docs.github.com/en/developers/apps/building-github-apps/authenticating-with-github-apps">Github OAuth</a> to retrieve your profile photo and add it to our Conference Directory. We will also generate a customized virtual ticket for you to share on social media!</p>
                     <div class="cta"><a href="https://github.com/login/oauth/authorize?client_id=${clientID}">Get Added to Directory</a></div>`
-                }
+                }`
+                : ``}
                 <h2>Need Help?</h2>
                 <p>Please contact us in Slack at #help-questions.</p>
                 <h2>Reset Session</h2>
