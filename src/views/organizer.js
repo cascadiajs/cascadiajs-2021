6aaa86309bfb50113c26c13518233b1439cb19bc
Carter Rabasa <carter.rabasa@gmail.com>
2021-09-15 09:54:27 -0700

    trying to slim down b/c Lambda file size limits

--- src/views/organizer.js
+++ src/views/organizer.js
@@ -40,9 +40,9 @@ module.exports = async function Organizer({organizer, social}) {
     }
     else {
         let content = Template(organizer)
-        let socialUrl = `/images/organizers/${ organizer.key }-social.png`
+        //let socialUrl = `/images/organizers/${ organizer.key }-social.png`
         let title = `${ organizer.name } | ${ organizer.role }`
-        html = Layout({ content, title, socialUrl })
+        html = Layout({ content, title })
     }
 
     return { html }
