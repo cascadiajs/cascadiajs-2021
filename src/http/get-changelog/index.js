ee9217cdb02b2774cbe1808624f86d0e02a434c1
Carter Rabasa <carter.rabasa@gmail.com>
2021-09-15 15:55:20 -0700

    fixed bug

--- src/http/get-changelog/index.js
+++ src/http/get-changelog/index.js
@@ -1,14 +1,14 @@
 // eslint-disable-next-line no-global-assign
 let arc = require('@architect/functions')
 let IndexView = require('@architect/views/changelog')
-let getChangelog = require('@architect/shared/changelog')
+let getChangelogData = require('@architect/shared/get-changelog-data')
 
 /**
  * This router passes the request to the appropriate view or static asset
  */
 async function Router () {
   // root (/) request, return Index view
-  let items = getChangelog()
+  let items = getChangelogData()
   return await IndexView(items)
 }
 
