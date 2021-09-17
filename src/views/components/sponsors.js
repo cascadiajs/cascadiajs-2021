d07f4f64d74197eed7bfb3c1bf14f81831d6e187
Carter Rabasa <carter.rabasa@gmail.com>
2021-09-17 14:44:35 -0700

    more sponsors

--- src/views/components/sponsors.js
+++ src/views/components/sponsors.js
@@ -2,9 +2,14 @@ let sponsors = require('../../shared/data/sponsors.json')
 
 module.exports = function SponsorsContainer () {
     return /*html*/`
-        <div class="sponsor-grid">
-        ${ sponsors.map(s => /*html*/`
-            <div><a href="/sponsors/${ s.key }"><img src="/images/sponsors/${ s.logo }" alt="${ s.name } logo"/></a></div>
-        `).join("")}
+        <div class="sponsors-grid">
+        ${ ['platinum', 'gold', 'silver', 'bronze'].map(tier => /*html*/`
+            ${ sponsors.filter(s => s.tier === tier).length > 0 ? /*html*/`
+                <div class="${ tier }">
+                ${ sponsors.filter(s => s.tier === tier).map(s => /*html*/`
+                    <div><a href="/sponsors/${ s.key }"><img src="/images/sponsors/${ s.logo }" alt="${ s.name } logo"/></a></div>`).join('')}
+                </div>
+            `: ''}        
+        `).join('')}
         </div>`
 }
