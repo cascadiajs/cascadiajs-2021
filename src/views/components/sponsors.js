eb04424b1b9d0412e3bb120fcc036b3bd0391525
Carter Rabasa <carter.rabasa@gmail.com>
2021-09-22 16:50:46 -0700

    added Courier sponsor

--- src/views/components/sponsors.js
+++ src/views/components/sponsors.js
@@ -5,6 +5,7 @@ module.exports = function SponsorsContainer () {
         <div class="sponsors-grid">
         ${ ['platinum', 'gold', 'silver', 'bronze'].map(tier => /*html*/`
             ${ sponsors.filter(s => s.tier === tier).length > 0 ? /*html*/`
+                <div class="tier-wrap"><div class="tier-inner"><div class="tier">${ tier }</div></div></div>
                 <div class="${ tier }">
                 ${ sponsors.filter(s => s.tier === tier).map(s => /*html*/`
                     <div><a href="/sponsors/${ s.key }"><img src="/images/sponsors/${ s.logo }" alt="${ s.name } logo"/></a></div>`).join('')}
