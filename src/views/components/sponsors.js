a3c522b9e801f74dee6a9289dd45110069edbc42
Carter Rabasa <carter.rabasa@gmail.com>
2021-10-11 14:15:27 -0700

    no link for bronze sponsors

--- src/views/components/sponsors.js
+++ src/views/components/sponsors.js
@@ -8,7 +8,7 @@ module.exports = function SponsorsContainer () {
                 <div class="tier-wrap"><div class="tier-inner"><div class="tier">${ tier }</div></div></div>
                 <div class="${ tier }">
                 ${ sponsors.filter(s => s.tier === tier).map(s => /*html*/`
-                    <div class="shake-slow"><a href="/sponsors/${ s.key }"><img src="/images/sponsors/${ s.logo }" alt="${ s.name } logo"/></a></div>`).join('')}
+                    <div class="shake-slow">${ tier !== 'bronze' ? `<a href="/sponsors/${ s.key }">` : '' }<img src="/images/sponsors/${ s.logo }" alt="${ s.name } logo"/>${ tier !== 'bronze' ? `</a>` : '' }</div>`).join('')}
                 </div>
             `: ''}        
         `).join('')}
