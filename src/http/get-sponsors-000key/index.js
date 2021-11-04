88e9c88ae47591513dc6c01bb472a7760c982309
Carter Rabasa <carter.rabasa@gmail.com>
2021-11-03 17:21:50 -0700

    debugging sponsor page

--- src/http/get-sponsors-000key/index.js
+++ src/http/get-sponsors-000key/index.js
@@ -4,7 +4,7 @@ let SponsorView = require('@architect/views/sponsor')
 let JobsView = require('@architect/views/jobs')
 let NotFoundView = require('@architect/views/404')
 
-async function Organizer(req) {
+async function Sponsor(req) {
   const { key } = req.params
   const { social } = req.queryStringParameters
   if (key === 'jobs') {
@@ -13,10 +13,11 @@ async function Organizer(req) {
   }
   else {
     const sponsor = sponsors.find(s => s.key === key)
+    console.log(sponsor)
     if (sponsor) {
       return await SponsorView({sponsor, social})
     }
   }
 }
 
-exports.handler = arc.http.async(Organizer, NotFoundView)
+exports.handler = arc.http.async(Sponsor, NotFoundView)
